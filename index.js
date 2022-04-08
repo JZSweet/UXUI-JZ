2fcbb400ed405dc0a538dcafeec5d0ca984f0204
JZ <73402906+JZSweet@users.noreply.github.com>
2022-04-08 12:55:28 -0700

    add uxui project page

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ img.onclick = () => {
         menu.style.display = "flex";
     }
 }
+
+function openWindow() {
+    window.open('./case/case1.html');
+};
+
+function openWindow2() {
+    window.open('./case/case2.html');
+};
+
+function openWindow3() {
+    window.open('./case/case3.html');
+};
+
+function closeWindow() {
+    window.close();
+}
